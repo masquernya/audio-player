02315e16e96b797822151eea114a508f6f087d0b
masquernya <127450910+masquernya@users.noreply.github.com>
2024-10-14 22:42:35 -0400

    use rpc so only one window is created

--- main.go
+++ main.go
@@ -1,79 +1,56 @@
 package main
 
 import (
-	"audio-player/audio"
 	"audio-player/ui"
-	"audio-player/visu"
-	"bytes"
 	"fmt"
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-	"image/color"
 	"log"
+	"net"
+	"net/http"
+	"net/rpc"
 	"os"
-	"path"
 )
 
-func playAudioSync(a *audio.Audio, s *ui.LayoutMain, bar fyne.CanvasObject, pos float32, dur float32) {
-	// setup cursor render
-	s.Play(bar, pos, dur)
-	// play
-	if err := a.Start(float64(pos)); err != nil {
-		log.Println("error starting audio:", err)
-	}
+type RpcServer struct {
+	u *ui.UI
+}
+
+func (r *RpcServer) PlayAudio(audioFile *string, reply *int) error {
+	fmt.Println("playing audio", audioFile)
+	r.u.Run(*audioFile)
+	return nil
 }
 
 func main() {
 	audioFile := os.Args[1]
 	fmt.Println("stating with", audioFile)
+	portStr := "19837"
 
-	l := &ui.LayoutMain{
-		PlaybackPercent: 0,
-	}
-
-	a := audio.New(audioFile)
-	dur := a.Duration()
-
-	imageBits, err := visu.GenerateImage(audioFile)
+	client, err := rpc.DialHTTP("tcp", "127.0.0.1:"+portStr)
 	if err != nil {
-		log.Fatal("error generating image:", err)
+		// fail, so setup server.
+		u := ui.New()
+		rpcServer := &RpcServer{u: u}
+		if err := rpc.Register(rpcServer); err != nil {
+			log.Fatal("error registering rpc:", err)
+		}
+		// start server
+		go (func() {
+			go rpc.HandleHTTP()
+			l, err := net.Listen("tcp", "127.0.0.1:"+portStr)
+			if err != nil {
+				log.Fatal("listen error:", err)
+			}
+			if err := http.Serve(l, nil); err != nil {
+				log.Fatal("error serving:", err)
+			}
+		})()
+
+		u.Run(audioFile)
+	} else {
+		var reply int
+		if err := client.Call("RpcServer.PlayAudio", &audioFile, &reply); err != nil {
+			log.Fatal("error calling rpc:", err)
+		}
+		log.Println("reply is", reply)
 	}
-
-	app := app.New()
-	w := app.NewWindow(path.Base(audioFile))
-	var ct *fyne.Container
-	cursor := canvas.NewRectangle(color.White)
-
-	image := canvas.NewImageFromReader(bytes.NewReader(imageBits), "waveform")
-	cursorPositioner := ui.NewClickableInvisible(func(event *fyne.PointEvent) {
-		log.Println("clicked at", event.Position)
-		// translate to duration
-		percent := event.Position.X / float32(image.Size().Width)
-		pos := dur * percent
-		log.Println("seeking to", pos)
-
-		playAudioSync(a, l, cursor, pos, dur)
-	})
-
-	replayButton := widget.NewButton("Replay", func() {
-		playAudioSync(a, l, cursor, 0, dur)
-	})
-
-	closeButton := widget.NewButton("Close", func() {
-		a.Stop()
-		app.Quit()
-	})
-
-	ct = container.New(l, image, cursorPositioner, replayButton, closeButton, cursor)
-
-	playAudioSync(a, l, cursor, 0, dur)
-
-	w.SetContent(ct)
-	w.ShowAndRun()
-
-	log.Println("app exit")
-	a.Stop()
 }
